04460f2176bfced45d8c8dfc578424a9eb61d6de
Aaron Lehmann <aaron.p.lehmann@gmail.com>
2024-07-27 01:36:40 -0400

    issue 8: Add unhappy path tests for get_page_class

--- tests/selenium_page_stubber/test_cli.py
+++ tests/selenium_page_stubber/test_cli.py
@@ -3,6 +3,7 @@ import pathlib
 import unittest.mock
 
 
+import jinja2
 import pytest
 import requests
 
@@ -31,9 +32,9 @@ def test_get_driver_bad_url(caplog: pytest.LogCaptureFixture) -> None:
             unittest.mock.patch("requests.get", return_value=resp),
             caplog.at_level(logging.ERROR)):
         selenium_page_stubber.cli.get_driver(url)
-        assert [msg for msg in caplog.record_tuples if msg == (
-            "root", logging.ERROR,
-            f"{requests.codes.not_found} status when GETting {url}")]
+    assert [msg for msg in caplog.record_tuples if msg == (
+        "root", logging.ERROR,
+        f"{requests.codes.not_found} status when GETting {url}")]
 
 
 def test_get_page_class_base_class_on_parent(
@@ -82,6 +83,30 @@ def test_get_page_class_from_module(tmp_path: pathlib.Path) -> None:
     assert issubclass(new_page_class, selenium_page_stubber.pages.Page.Page)
 
 
+@pytest.mark.parametrize(
+    ["module_text", "error"], (
+        [")", SyntaxError],
+        ["5 / 0", ZeroDivisionError]
+    ))
+def test_get_page_class_from_module_error(
+        tmp_path: pathlib.Path,
+        module_text: str, error: Exception) -> None:
+    page_directory = tmp_path
+    page_module = "module"
+    page_class = "TestPage"
+    template_directory = pathlib.Path("template_directory")
+
+    with (page_directory / "{}.py".format(page_module)).open("w") as module:
+        module.write(module_text)
+
+    with pytest.raises(error):  # type: ignore[call-overload]
+        selenium_page_stubber.cli.get_page_class(
+            page_directory=page_directory,
+            page_module=page_module,
+            page_class=page_class,
+            template_directory=template_directory)
+
+
 def test_get_page_class_from_template(tmp_path: pathlib.Path) -> None:
     page_directory = pathlib.Path("path_directory")
     page_module = "module"
@@ -105,3 +130,26 @@ def test_get_page_class_from_template(tmp_path: pathlib.Path) -> None:
 
     assert new_page_class.__name__ == page_class
     assert issubclass(new_page_class, selenium_page_stubber.pages.Page.Page)
+
+
+@pytest.mark.parametrize(
+    ["template_text", "error"], (
+        ["{% if %}", jinja2.TemplateSyntaxError],
+        ["5 / 0", ZeroDivisionError]
+    ))
+def test_get_page_class_from_template_error(
+        tmp_path: pathlib.Path, template_text: str, error: Exception) -> None:
+    page_directory = pathlib.Path("path_directory")
+    page_module = "module"
+    page_class = "TestPage"
+    template_directory = tmp_path
+
+    with (template_directory / page_class).open("w") as template:
+        template.write(template_text)
+
+    with pytest.raises(error):  # type: ignore[call-overload]
+        selenium_page_stubber.cli.get_page_class(
+            page_directory=page_directory,
+            page_module=page_module,
+            page_class=page_class,
+            template_directory=template_directory)
